6fec9d505ebd4ce0b4b00f8b1bd0dcee6d3be1a7
Yaroslav Gaiderov <gainer495@gmail.com>
2025-11-04 22:26:09 +0300

    ci: add ci script
    Signed-off-by: Yaroslav Gaiderov <gainer495@gmail.com>

--- test/Task5Spec.hs
+++ test/Task5Spec.hs
@@ -1,25 +1,25 @@
 module Task5Spec (tests) where
 
-import Test.HUnit
 import Task5
-  ( divisibleByAll
-  , divisibleByAllTail
-  , smallestMultipleRec
-  , smallestMultipleTailRec
-  , leastCommonMul
-  , leastCommonMulMap2
-  , leastCommonMulFor
+  ( divisibleByAll,
+    divisibleByAllTail,
+    leastCommonMul,
+    leastCommonMulFor,
+    leastCommonMulMap2,
+    smallestMultipleRec,
+    smallestMultipleTailRec,
   )
+import Test.HUnit
 
 tests :: IO Counts
-tests = runTestTT $ TestList
-  [ "divisibleByAll 12 4 == True"        ~: True  ~=? divisibleByAll 12 4
-  , "divisibleByAllTail 12 5 == False"   ~: False ~=? divisibleByAllTail 12 5
-
-  , "smallestMultipleRec 10 == 2520"     ~: 2520  ~=? smallestMultipleRec 10
-  , "smallestMultipleTailRec 10 == 2520" ~: 2520  ~=? smallestMultipleTailRec 10
-
-  , "leastCommonMul 10 == 2520"          ~: 2520  ~=? leastCommonMul 10
-  , "leastCommonMulMap2 10 == 2520"      ~: 2520  ~=? leastCommonMulMap2 10
-  , "leastCommonMulFor 10 == 2520"       ~: 2520  ~=? leastCommonMulFor 10
-  ]
+tests =
+  runTestTT $
+    TestList
+      [ "divisibleByAll 12 4 == True" ~: True ~=? divisibleByAll 12 4,
+        "divisibleByAllTail 12 5 == False" ~: False ~=? divisibleByAllTail 12 5,
+        "smallestMultipleRec 10 == 2520" ~: 2520 ~=? smallestMultipleRec 10,
+        "smallestMultipleTailRec 10 == 2520" ~: 2520 ~=? smallestMultipleTailRec 10,
+        "leastCommonMul 10 == 2520" ~: 2520 ~=? leastCommonMul 10,
+        "leastCommonMulMap2 10 == 2520" ~: 2520 ~=? leastCommonMulMap2 10,
+        "leastCommonMulFor 10 == 2520" ~: 2520 ~=? leastCommonMulFor 10
+      ]
